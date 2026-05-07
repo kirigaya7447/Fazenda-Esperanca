9143e5ae4ba2747f850c21774f1967a4255688d0
p3dr0wzt <pedrohenriquesilvamoreira0@gmail.com>
2026-05-07 19:51:56 -0300

    adicionando a main

--- sistemaFazendaEsperanca/src/App.java
+++ sistemaFazendaEsperanca/src/App.java
@@ -1,5 +1,62 @@
+import java.util.Scanner;
 public class App {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int opcao;
+        do {
+            System.out.println("----> SISTEMA FAZENDA <----");
+            System.out.println("1 - Cadastrar Funcionário");
+            System.out.println("2 - Cadastrar Talhão");
+            System.out.println("3 - Cadastrar Trator");
+            System.out.println("4 - Registrar Entrada de Café");
+            System.out.println("5 - Relatório Quinzena");
+            System.out.println("6 - Relatório Talhão");
+            System.out.println("7 - Relatório Secagem");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha: ");
+
+            opcao = sc.nextInt();
+            sc.nextLine();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    cadastrarFuncionario();
+                    break;
+
+                case 2:
+                    cadastrarTalhao();
+                    break;
+
+                case 3:
+                    cadastrarTrator();
+                    break;
+
+                case 4:
+                    registrarCafe();
+                    break;
+
+                case 5:
+                    relatorioQuinzena();
+                    break;
+
+                case 6:
+                    relatorioTalhao();
+                    break;
+
+                case 7:
+                    relatorioSecagem();
+                    break;
+
+                case 0:
+                    System.out.println("Sistema encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+    }
     }
 }
